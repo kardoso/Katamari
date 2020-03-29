ae8bfe30a28e11c76aa4b8a28c640d8659cfc180
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-03-29 11:15:14 -0300

    Add ball controller

---
+++ Assets/Scripts/Ball/StickyBall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class StickyBall : MonoBehaviour
+{
+	float facingAngle = 0;
+	float x = 0;
+	float z = 0;
+	public Vector2 facingDirectionVector;
+
+	void Update ()
+	{
+		UpdateInput ();
+	}
+
+	void FixedUpdate () {
+		Move();
+	}
+
+	void Move ()
+	{
+		transform.GetComponent<Rigidbody> ().AddForce (
+			new Vector3 (
+				facingDirectionVector.x,
+				0,
+				facingDirectionVector.y
+			) * z * 2
+		);
+	}
+
+	void UpdateInput ()
+	{
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime * -100;
+		z = Input.GetAxis ("Vertical") * Time.deltaTime * 500;
+
+		facingAngle += x;
+		facingDirectionVector = new Vector2 (
+			Mathf.Cos (facingAngle * Mathf.Deg2Rad),
+			Mathf.Sin (facingAngle * Mathf.Deg2Rad)
+		);
+	}
+}
