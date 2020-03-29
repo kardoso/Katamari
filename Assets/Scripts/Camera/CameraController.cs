de9e0fc28dca991ecf1bb3d926605a1ea82cad74
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-03-29 10:24:55 -0300

    Add Camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 lookAtOffset;
+
+    void Start()
+    {
+        lookAtOffset = new Vector3(0, 1.5f, 0);
+    }
+
+    void Update()
+    {
+        var ballPos = ball.transform.position;
+        transform.LookAt(ballPos + lookAtOffset);
+    }
+
+    void FixedUpdate()
+    {
+        var ballPos = ball.transform.position;
+        transform.position = new Vector3(ballPos.x, ballPos.y + 4, ballPos.z - 4);
+    }
+}
